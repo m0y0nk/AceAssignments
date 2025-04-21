8abf79630ed331e75aad717700aaa9c3b38b58c3
Mayank <Myank>
2025-04-21 16:13:57 +0530

    added logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import SearchBar from "./SearchBar.jsx";
-import SearchQueryProvider from "../../context/SearchQuery.jsx";
 import './Navbar.css';
-// import Logo from "../logo/Logo";
+import Logo from "../logo/logo.png";
 
 export default function Navbar() {
   return (
     <>
       <nav className="flex items-center justify-between w-full px-8 py-4 bg-white shadow-md">
         <div className="text-2xl font-bold text-gray-800">
-          {/* <Logo /> */}
-          logo..
+          <Link to="/" className="text-gray-800 hover:text-blue-600 transition duration-200">
+            <img src={Logo} alt="Logo" className="h-10 w-10 inline-block mr-2" />
+            AceAssignments
+          </Link>
         </div>
 
         <div className="flex items-center space-x-8">
-          <div className="">
-              <SearchQueryProvider>
-                <SearchBar />
-              </SearchQueryProvider>
-          </div>
-
           <div className="flex space-x-6 text-lg font-medium">
             <Link
               to="/"
